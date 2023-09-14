39cd3e2db3a11873eeee60317f1baf940f315621
Vibhu Gupta <vik6026717961@gmail.com>
2023-09-14 17:05:28 +0530

    new Form Project Started

---
+++ form_project/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css';
+
+function App() {
+
+  function changeFirstNameHAndeler (event) {
+    console.log(event.target.value);
+
+    // intermi 15:00
+  }
+
+
+  function changeLastNameHAndeler (event) {
+    console.log(event.target.value);
+  }
+
+  return (
+    <div>
+      <form >
+        <input type="text"
+        placeholder='First Name'
+        onChange={changeFirstNameHAndeler}
+        />
+
+       <input type="text"
+        placeholder='Last Name'
+        onChange={changeLastNameHAndeler}
+        /> 
+      </form>
+    </div>
+  );
+}
+
+export default App;
